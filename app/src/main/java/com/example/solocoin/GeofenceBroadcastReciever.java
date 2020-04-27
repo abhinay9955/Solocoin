c8a3d7b6605eadf7563ec7995ffc70e3873055c9
abhinay9955 <kumarabhinay599@gmail.com>
2020-04-27 20:17:26 +0530

    geofencing added

--- app/src/main/java/com/example/solocoin/GeofenceBroadcastReciever.java
+++ app/src/main/java/com/example/solocoin/GeofenceBroadcastReciever.java
@@ -3,13 +3,54 @@ package com.example.solocoin;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofenceStatusCodes;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class GeofenceBroadcastReciever extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.i("recieved", "onReceive: ");
+        int x = context.getSharedPreferences("location",Context.MODE_PRIVATE).getInt("count",0);
+        context.getSharedPreferences("location",Context.MODE_PRIVATE).edit().putInt("count",x+1);
+        context.getSharedPreferences("location",Context.MODE_PRIVATE).edit().apply();
+        Toast.makeText(context,"Recieved",Toast.LENGTH_LONG).show();
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            String errorMessage =GeofenceStatusCodes.getStatusCodeString(geofencingEvent.getErrorCode());
+            Log.i("error", errorMessage);
+            return;
+        }
+
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            // Get the transition details as a String.
+
+
+            // Send notification and log the transition details.
+            Log.i("onReceive: ","changed");
+        } else {
+            // Log the error.
+            Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
+
--- app/src/main/java/com/example/solocoin/MapsActivity.java
+++ app/src/main/java/com/example/solocoin/MapsActivity.java
@@ -13,6 +13,8 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,32 +25,46 @@ import android.widget.Toast;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
     private PendingIntent geofencePendingIntent;
     private GeofencingClient geofencingClient;
     private Button start,stop;
-    Marker geoMarker;
+    private Marker geoMarker,curMarker;
+    private Circle circle;
     private ArrayList<Geofence> geofenceList;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    Double lat,lng ; // latitude and longitude of already present geofence
+    Boolean isGeofence; //check if there is already a geofence
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        sharedPreferences = getSharedPreferences("location",MODE_PRIVATE);
+        editor = sharedPreferences.edit();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -59,69 +75,46 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         stop.setOnClickListener(this);
         geofencingClient = LocationServices.getGeofencingClient(this);
         geofenceList = new ArrayList<>();
-    }
+        isGeofence = sharedPreferences.getBoolean("geofence",false);
+        Toast.makeText(this,""+isGeofence,Toast.LENGTH_LONG).show();
+        if(isGeofence)
+        {
+            lat = Double.parseDouble(sharedPreferences.getString("lat","0.0"));
+            lng = Double.parseDouble(sharedPreferences.getString("lng","0.0"));
+            if(mMap!=null)
+            {
+                if(geoMarker!=null)
+                    geoMarker.remove();
+               geoMarker= mMap.addMarker(new MarkerOptions().position(new LatLng(lat,lng)).title("Geofence Marker"));
+            }
+        }
 
+    }
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
         mMap.setOnMapClickListener(this);
-        mMap.setOnMarkerClickListener(this);
-
-    }
-
-
-
-
-
-    private PendingIntent getGeofencePendingIntent() {
-        if (geofencePendingIntent != null) {
-            return geofencePendingIntent;
-        }
-        Intent intent = new Intent(this, GeofenceBroadcastReciever.class);
-
-        geofencePendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.
-                FLAG_UPDATE_CURRENT);
-        return geofencePendingIntent;
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        switch (v.getId()) {
-            case R.id.start:  if (geoMarker == null || geofenceList.size() == 0) {
-                                   Toast.makeText(MapsActivity.this, "Select a Geofence marker", Toast.LENGTH_SHORT).show();
-                                    return;
-                               }
-                               geofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                 @Override
-                                 public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                       Toast.makeText(MapsActivity.this, "Successful", Toast.LENGTH_SHORT).show();
-                                    } else {
-                                        Toast.makeText(MapsActivity.this, "Unable to add Geofence", Toast.LENGTH_SHORT).show();
-                                      }
-                                  }
-                               });
-                               break;
-
-
-
-            case R.id.stop :  geofencingClient.removeGeofences(getGeofencePendingIntent()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if(task.isSuccessful())
-                    {
-                        Toast.makeText(MapsActivity.this, "Successfully removed Geofence", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        Toast.makeText(MapsActivity.this, "Error removing Geofence", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-            break;
+        isGeofence = sharedPreferences.getBoolean("geofence",false);
+        Toast.makeText(this,""+isGeofence,Toast.LENGTH_LONG).show();
+        if(isGeofence)
+        {
+            lat = Double.parseDouble(sharedPreferences.getString("lat","0.0"));
+            lng = Double.parseDouble(sharedPreferences.getString("lng","0.0"));
+            if(mMap!=null)
+            {
+                if(geoMarker!=null)
+                    geoMarker.remove();
+                geoMarker= mMap.addMarker(new MarkerOptions().position(new LatLng(lat,lng)).title("Geofence Marker"));
+            }
+            CircleOptions circleOptions =new CircleOptions().center(new LatLng(lat,lng)).radius(100f).strokeColor(Color.parseColor("#543535")).fillColor(Color.parseColor("#a44dc6"));
+            if(circle!=null)
+                circle.remove();
+            circle = mMap.addCircle(circleOptions);
+            float zoomLevel = 16.0f; //This goes up to 21
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat,lng), zoomLevel));
         }
     }
 
@@ -130,38 +123,132 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         if(mMap!=null) {
 
             MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Geofence Marker");
-
+            CircleOptions circleOptions =new CircleOptions().center(latLng).radius(100f).strokeColor(Color.parseColor("#543535")).fillColor(Color.parseColor("#a44dc6"));
             if (geoMarker != null) {
                 geoMarker.remove();
             }
+            if(circle!=null)
+                circle.remove();
             geoMarker = mMap.addMarker(markerOptions);
-            geofenceList.clear();
-            geofenceList.add(new Geofence.Builder()
-                    // Set the request ID of the geofence. This is a string to identify this
-                    // geofence.
-                    .setRequestId("GEOFENCEREQUEST")
+            circle = mMap.addCircle(circleOptions);
 
-                    .setCircularRegion(
-                            geoMarker.getPosition().latitude,
-                            geoMarker.getPosition().longitude,
-                            100
-                    )
-                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
-                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
-                            Geofence.GEOFENCE_TRANSITION_EXIT)
-                    .build());
         }
     }
 
+
     @Override
-    public boolean onMarkerClick(Marker marker) {
-        return false;
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.start:  if (geoMarker == null) {
+                               Toast.makeText(MapsActivity.this, "Select a Geofence marker", Toast.LENGTH_SHORT).show();
+                               break;
+                               }
+                              addGeofences();
+                              break;
+
+            case R.id.stop :  stopGeofence();
+                              break;
+        }
     }
+
+
+
+    private void addGeofences(){
+        geofencingClient.addGeofences(getGeofencingRequest(),getGeofencePendingIntent()).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                   Toast.makeText(MapsActivity.this,"Successfully added geofences",Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MapsActivity.this,"Error in adding geofences",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    private PendingIntent getGeofencePendingIntent() {
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+        Intent intent = new Intent(MapsActivity.this, GeofenceBroadcastReciever.class);
+        geofencePendingIntent = PendingIntent.getBroadcast(MapsActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
+    }
+
+
+
+
+    private void stopGeofence(){
+        geofencingClient.removeGeofences(getGeofencePendingIntent()).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                   Toast.makeText(MapsActivity.this,"Successfully removed",Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                   Toast.makeText(MapsActivity.this,"Failed removing",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+
+   private Geofence getGeofence(){
+        return  new Geofence.Builder()
+                .setCircularRegion(geoMarker.getPosition().latitude,geoMarker.getPosition().longitude,100f)
+                .setRequestId("GeofenceID")
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT |Geofence.GEOFENCE_TRANSITION_DWELL)
+                .setLoiteringDelay(5000)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .build();
+   }
 
     private GeofencingRequest getGeofencingRequest() {
         GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
-        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL);
-        builder.addGeofences(geofenceList);
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofence(getGeofence());
         return builder.build();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+
+
+
+        editor.apply();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("stop", "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "Destroyed", Toast.LENGTH_SHORT).show();
+        Log.i("dest", "onDestroy: ");
+        if(geoMarker!=null)
+        {
+            editor.putBoolean("geofence",true);
+            editor.putString("lat",String.valueOf(geoMarker.getPosition().latitude));
+            editor.putString("lng",String.valueOf(geoMarker.getPosition().longitude));
+        }
+        else
+        {
+            editor.putBoolean("geofence",false);
+            editor.putString("lat",String.valueOf(0.0));
+            editor.putString("lng",String.valueOf(0.0));
+
+        }
+        editor.apply();
+        super.onDestroy();
     }
 }
